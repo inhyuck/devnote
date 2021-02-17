c84bab8d003735e8b7ecaab1ce3b676e7d900e2c
inhyuck choi <dlsgur1447@gmail.com>
2021-02-17 23:48:43 +0900

    study(spring) : [코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술] 스프링 데이터 Jpa

--- spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -13,25 +13,16 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
 }
