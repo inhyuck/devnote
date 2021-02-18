58c9506cf62a943ff273542c02f8be35e7e137d8
inhyuck choi <dlsgur1447@gmail.com>
2021-02-18 21:28:30 +0900

    study(spring) : [코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술] AOP 적용

--- spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService " + memberService.getClass()); //memberService Proxy 가 주입되는지 확인
     }
 
     @GetMapping("members/new")
--- spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -20,19 +20,9 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            this.validateDuplicateMember(member); //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join " + timeMs + "ms");
-        }
-
+        this.validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -46,16 +36,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
