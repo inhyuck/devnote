343c2ffea75c64848b5b1d1d6c987fd941e58255
inhyuck choi <dlsgur1447@gmail.com>
2021-02-18 20:53:16 +0900

    study(spring) : [코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술] AOP 필요한 상황

--- spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -21,9 +21,18 @@ public class MemberService {
      */
     public Long join(Member member) {
 
-        this.validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            this.validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -37,7 +46,16 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
